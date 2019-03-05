fe900f91f115d0a3a9be206df3b1c8b4f9a1da95
小天涯 <1731327638@qq.com>
2019-03-05 11:08:30 +0800

    主页修改

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-var scrollUp = document.getElementById('scrollUp');
+var scrollUp = document.getElementById('scrollUp'),
+    Dm = document.getElementsByClassName('dm-equipment')[0],
+    Lb = document.getElementsByClassName('lb-wrapper')[0];
 
 window.onscroll = function() {
     var scrollTop = document.documentElement.scrollTop;
@@ -8,4 +10,11 @@ window.onscroll = function() {
         scrollUp.className = 'scroll';
     }
     console.log(scrollTop);
+    if(scrollTop >= 2500){
+        // Dm.style.display = 'block';
+        Dm.style.transform = 'translateX(0)';
+    }
+    if(scrollTop >= 150){
+        // Lb.style.transform = 'translateX(0)';
+    }
 }
